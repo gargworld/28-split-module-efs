a06511896fb4090e51f1dc8c3288f8cff0d80eb0
gargworld <emuser@gmail.com>
2025-06-21 21:55:01 -0500

    added more logs to index.py lambda

--- modules/eventbridge/lambda/index.py
+++ modules/eventbridge/lambda/index.py
@@ -1,10 +1,15 @@
-import os
+mport os
 import boto3
 
 def lambda_handler(event, context):
-    project_name = os.environ['PROJECT_NAME']
-    client = boto3.client('codebuild')
+    print("Lambda triggered with event:", event)
 
-    response = client.start_build(projectName=project_name)
-    print(f"Started CodeBuild: {response['build']['id']}")
-
+    try:
+        project_name = os.environ['PROJECT_NAME']
+        print(f"Starting CodeBuild project: {project_name}")
+        client = boto3.client('codebuild')
+        response = client.start_build(projectName=project_name)
+        print(f"Started CodeBuild: {response['build']['id']}")
+    except Exception as e:
+        print("Error:", str(e))
+        raise
