10f14a257551d72b0bd101ea3f483418d85a82dc
gargworld <emuser@gmail.com>
2025-06-26 21:49:38 -0500

    update ec2 without ansible folder update

--- old/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket-704630444454"
-    key            = "infra/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
